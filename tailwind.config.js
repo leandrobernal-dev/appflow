867bdbe40fa726d50742a4523f18a440bd1cb69f
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-20 08:48:29 +0800

    added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        colors: {
+            "text-light": "#1C171A",
+            "text-dark": "#f8f7f7",
+            "background-light": "#F8F7F7",
+            "background-dark": "#1C171A",
+            primary: "#554951",
+            "secondary-light": "#E2DADE",
+            "secondary-dark": "#141012",
+            "accent-light": "#826d7a",
+            "accent-dark": "#C9BFC5",
+            "accent-color": "#C9BFC5",
+        },
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+    darkMode: "class",
+};
