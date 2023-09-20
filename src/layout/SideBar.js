b134dc31545225187a37ec39d191df89ff25e0e4
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-20 21:05:31 +0800

    created SideBar

---
+++ src/layout/SideBar.js
@@ -0,0 +1,46 @@
+import {
+	TrendingUpRounded,
+	TuneRounded,
+	ViewKanbanRounded,
+	WorkspacesRounded,
+} from "@mui/icons-material";
+
+export default function Sidebar() {
+	const sideBarButtons = [
+		{
+			name: "Teams",
+			icon: <WorkspacesRounded />,
+			active: true,
+		},
+		{
+			name: "Kanban",
+			icon: <ViewKanbanRounded />,
+		},
+		{
+			name: "Settings",
+			icon: <TuneRounded />,
+		},
+		{
+			name: "Project Activity",
+			icon: <TrendingUpRounded />,
+		},
+	];
+	return (
+		<aside className="fixed bottom-0 left-0 top-0 w-48 border-r border-accent-light bg-secondary-light p-4 pt-24 dark:border-accent-dark dark:bg-secondary-dark">
+			<div className="flex flex-col gap-6 py-4">
+				{sideBarButtons.map((button, index) => {
+					return (
+						<button
+							key={button.name + index}
+							className={`flex w-full items-center gap-2 rounded-lg text-text-light dark:text-text-dark ${
+								button.active ? "font-black" : "font-medium"
+							}`}
+						>
+							{button.icon} <span>{button.name}</span>
+						</button>
+					);
+				})}
+			</div>
+		</aside>
+	);
+}
