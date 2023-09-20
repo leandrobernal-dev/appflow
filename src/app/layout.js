81fb1594aec275d63e9f51724a2c01c0693b31ee
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-20 21:04:53 +0800

    removed warnings

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,18 +4,19 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "AppFlow",
-    description: "Project Management tool for App/WebApp Developers",
+	title: "AppFlow",
+	description: "Project Management tool for App/WebApp Developers",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body
-                className={`${inter.className} bg-background-light dark:bg-background-dark`}
-            >
-                {children}
-            </body>
-        </html>
-    );
+	return (
+		<html lang="en" className="">
+			<body
+				suppressHydrationWarning={true}
+				className={`${inter.className} bg-background-light dark:bg-background-dark`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
