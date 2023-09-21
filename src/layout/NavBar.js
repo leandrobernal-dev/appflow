eaa5947becfef22ec22a2898548c65f3f73493ea
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-21 14:19:06 +0800

    moved navbuttons on its own component

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,31 +1,20 @@
-import {
-	AccountCircleRounded,
-	ArrowDropDownOutlined,
-	DarkModeOutlined,
-	NotificationsRounded,
-} from "@mui/icons-material";
+import NavButtons from "@/components/NavButtons";
+import { MenuRounded } from "@mui/icons-material";
 
 export default function NavBar() {
 	return (
-		<nav className="fixed left-0 right-0 top-0 flex h-20 justify-between border-b border-accent-light p-4 dark:border-accent-dark">
+		<nav className="fixed left-0 right-0 top-0 z-50 flex h-20 justify-between border-b border-accent-light p-4 dark:border-accent-dark">
 			<a href="/account">
 				<img src="/logo.svg" className="h-full" alt="AppFlow logo" />
 			</a>
 
-			<div className="flex items-center justify-between gap-2">
+			<div className="flex items-center sm:hidden">
 				<button className="rounded-lg bg-secondary-light p-2 ">
-					<NotificationsRounded />
+					<MenuRounded />
 				</button>
-				<button className="rounded-lg bg-primary p-2 text-text-dark dark:text-text-light">
-					<span>Select Project</span>
-					<ArrowDropDownOutlined />
-				</button>
-				<button className="rounded-lg bg-secondary-dark p-2 text-text-dark dark:bg-secondary-light">
-					<DarkModeOutlined />
-				</button>
-				<button className="rounded-lg bg-secondary-light p-2">
-					<AccountCircleRounded />
-				</button>
+			</div>
+			<div className="hidden items-center justify-between gap-2 sm:flex">
+				<NavButtons />
 			</div>
 		</nav>
 	);
