15223403f392ba1672777380edfc7b0875d9cfd8
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-21 21:23:27 +0800

    created a newproject form

---
+++ src/app/account/newproject/page.js
@@ -0,0 +1,76 @@
+"use client";
+
+import { UserDataContext } from "@/context/UserDataContext";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
+
+export default function NewProjectPage() {
+	const router = useRouter();
+	const { setActiveProject, setProjects } = useContext(UserDataContext);
+
+	function handleNewProjectSubmit(e) {
+		e.preventDefault();
+		const { name, description } = e.currentTarget.elements;
+		axios
+			.post("/api/project", {
+				name: name.value,
+				description: description.value,
+			})
+			.then((response) => {
+				const data = response.data;
+				console.log(data);
+				setActiveProject(data.data);
+				setProjects((prevProjects) => {
+					const newProjects = [...prevProjects];
+					newProjects.push(data.data);
+					return newProjects;
+				});
+				if (data.data) {
+					localStorage.setItem(
+						"activeProject",
+						JSON.stringify(data.data),
+					);
+				}
+				router.push("/account");
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+	return (
+		<form
+			onSubmit={handleNewProjectSubmit}
+			className="flex w-full max-w-2xl flex-col gap-2 p-6"
+		>
+			<h1 className="py-4 text-3xl font-black">New Project</h1>
+			<div>
+				<label htmlFor="new-project-name" className="block">
+					Project Name
+				</label>
+				<input
+					type="text"
+					id="new-project-name"
+					autoFocus
+					name="name"
+					className="w-full rounded-lg bg-secondary-light"
+				/>
+			</div>
+			<div>
+				<label htmlFor="new-project-description" className="block">
+					Description
+				</label>
+				<input
+					type="text"
+					name="description"
+					id="new-project-description"
+					className="w-full rounded-lg bg-secondary-light"
+				/>
+			</div>
+
+			<button className="rounded-lg bg-primary p-2 text-text-dark">
+				Create Project
+			</button>
+		</form>
+	);
+}
