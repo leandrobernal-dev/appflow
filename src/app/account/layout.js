f8fe25cdfbb1bad707ca5d234fe9c08d39f089ca
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-21 14:17:53 +0800

    minor ui adjustment

--- src/app/account/layout.js
+++ src/app/account/layout.js
@@ -1,11 +1,14 @@
+"use client";
+
 import NavBar from "@/layout/NavBar";
 import Sidebar from "@/layout/SideBar";
 
 export default function AccountLayout({ children }) {
 	return (
-		<main className="">
+		<main className="pt-20 sm:pl-48 ">
 			<Sidebar />
 			<NavBar />
+
 			{children}
 		</main>
 	);
