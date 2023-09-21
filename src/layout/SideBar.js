134d8736b6debf5f6f92d1eb24e5b1bd889efe62
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-21 14:18:37 +0800

    added nav buttons to sidebar on mobile view

--- src/layout/SideBar.js
+++ src/layout/SideBar.js
@@ -1,3 +1,4 @@
+import NavButtons from "@/components/NavButtons";
 import {
 	TrendingUpRounded,
 	TuneRounded,
@@ -26,7 +27,10 @@ export default function Sidebar() {
 		},
 	];
 	return (
-		<aside className="fixed bottom-0 left-0 top-0 w-48 border-r border-accent-light bg-secondary-light p-4 pt-24 dark:border-accent-dark dark:bg-secondary-dark">
+		<aside className="fixed bottom-0 left-0 right-0 top-0 z-40 border-accent-light bg-text-dark p-4 pt-24 dark:border-accent-dark dark:bg-secondary-dark sm:block sm:w-48 sm:border-r sm:bg-secondary-light">
+			<div className="flex items-center gap-4 sm:hidden">
+				<NavButtons />
+			</div>
 			<div className="flex flex-col gap-6 py-4">
 				{sideBarButtons.map((button, index) => {
 					return (
