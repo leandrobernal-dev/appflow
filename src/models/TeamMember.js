08b881ad6529fd2d4a9be9b1316431d26160b731
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-25 06:40:30 +0800

    created TeamMember model

---
+++ src/models/TeamMember.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const teamMemberDb = mongoose.connection.useDb("TeamMembers");
+const TeamMemberSchema = new Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		project: {
+			type: Schema.Types.ObjectId,
+			ref: "Project",
+			required: true,
+		},
+		team: {
+			type: Schema.Types.ObjectId,
+			ref: "Team",
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	},
+);
+
+const TeamMember = teamMemberDb.model("TeamMembers", TeamMemberSchema);
+export default TeamMember;
