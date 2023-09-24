d7495539eb846e3334bb7a3c995fb07222903e81
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-25 06:44:16 +0800

    added new project logic, created delete route

--- src/app/api/project/route.js
+++ src/app/api/project/route.js
@@ -1,5 +1,7 @@
 import dbConnect from "@/db/database";
 import Project from "@/models/Project";
+import ProjectMember from "@/models/ProjectMember";
+import Team from "@/models/Team";
 import User from "@/models/User";
 import { getServerSession } from "next-auth";
 
@@ -10,12 +12,20 @@ export const GET = async () => {
 
 	try {
 		const { user } = await getServerSession();
+		const account = await User.findOne({ email: user.email });
+
 		try {
-			const data = await User.findOne({ email: user.email }).populate(
-				"projects",
-			);
+			const projects = await ProjectMember.find({
+				user: account,
+			}).populate({
+				path: "project",
+				model: Project,
+			});
+
+			const data = { projects };
 			return NextResponse.json({ data });
 		} catch (error) {
+			console.log(error);
 			return NextResponse.json({ msg: error.message }, { status: 500 });
 		}
 	} catch (error) {
@@ -36,15 +46,49 @@ export const POST = async (request) => {
 
 		const { name, description } = await request.json();
 		const newProject = new Project({
-			user: account._id,
+			admin: account._id,
 			name,
 			description,
 		});
 		await newProject.save();
+		const newProjectMember = new ProjectMember({
+			project: newProject,
+			user: account._id,
+			role: "admin",
+		});
+		await newProjectMember.save();
 
-		return NextResponse.json({ msg: "Success", data: newProject });
+		const project = await ProjectMember.findOne({
+			user: account,
+			project: newProject,
+		}).populate({
+			path: "project",
+			model: Project,
+		});
+		console.log(project);
+
+		return NextResponse.json({ msg: "Success", data: project });
 	} catch (error) {
 		console.log(error);
 		return NextResponse.json({ error }, { status: 500 });
+	}
+};
+
+export const DELETE = async (request) => {
+	await dbConnect();
+
+	try {
+		const { user } = await getServerSession();
+		const { searchParams } = new URL(request.url);
+		const id = searchParams.get("id").toString();
+
+		const project = await Project.findById(id);
+		await Team.deleteMany({ project });
+		await ProjectMember.deleteMany({ project });
+		await project.deleteOne();
+
+		return NextResponse.json({ msg: "Project deleted Successfully" });
+	} catch (error) {
+		console.log(error);
 	}
 };
