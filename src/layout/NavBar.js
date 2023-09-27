16848c8a01ad86a98290e7cc8b876e5b3605cf94
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-09-27 22:07:57 +0800

    replaced a tags with Link tags

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,12 +1,13 @@
 import NavButtons from "@/components/NavButtons";
 import { MenuRounded } from "@mui/icons-material";
+import Link from "next/link";
 
 export default function NavBar() {
 	return (
 		<nav className="fixed left-0 right-0 top-0 z-40 flex h-20 justify-between border-b border-accent-light p-4 dark:border-accent-dark">
-			<a href="/account">
+			<Link href="/account">
 				<img src="/logo.svg" className="h-full" alt="AppFlow logo" />
-			</a>
+			</Link>
 
 			<div className="flex items-center sm:hidden">
 				<button className="rounded-lg bg-secondary-light p-2 ">
