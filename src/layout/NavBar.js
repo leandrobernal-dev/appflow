b47267841dbfc267b3d669390be19c033cc4d23b
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-10-03 18:12:41 +0800

    updated navbar layout/design

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function NavBar() {
 	return (
-		<nav className="fixed left-0 right-0 top-0 z-40 flex h-20 justify-between border-b border-accent-light/30 p-4 dark:border-accent-dark">
+		<nav className="bg-primary-background-light shadow-shadow-light fixed left-0 right-0 top-0 z-40 flex h-20 justify-between border-b border-accent-light/20 p-4 shadow-md	">
 			<Link href="/account">
 				<img src="/logo.svg" className="h-full" alt="AppFlow logo" />
 			</Link>
@@ -14,7 +14,7 @@ export default function NavBar() {
 					<MenuRounded />
 				</button>
 			</div>
-			<div className="hidden items-center justify-between gap-2 sm:flex">
+			<div className="hidden items-center justify-between gap-4 sm:flex">
 				<NavButtons isMobile={false} />
 			</div>
 		</nav>
