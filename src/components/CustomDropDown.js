12bf2cf764f1d04589cea15de3cac1bed66a08ac
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2023-10-03 18:13:08 +0800

    minor theme adjustment

--- src/components/CustomDropDown.js
+++ src/components/CustomDropDown.js
@@ -1,7 +1,7 @@
 export default function CustomDropDown({ id, children }) {
 	return (
 		<div
-			className="z-10 hidden w-52  divide-y divide-gray-100 rounded-lg bg-secondary-light shadow"
+			className="shadow-shadow-light z-10 hidden  w-52 divide-y divide-gray-100 rounded-lg border border-accent-light/20 bg-white shadow-lg"
 			id={id}
 		>
 			{children}
